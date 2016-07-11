8978514a9d4c2d03c92dcf02ddc1da9d2ce84009
zhangjiangwei <h5zzbox@gmail.com>
2016-07-11 17:22:27 +0800

    B.加了个小的画画

--- gameweb/src/view/StatusLayer.js
+++ gameweb/src/view/StatusLayer.js
@@ -1,11 +1,11 @@
 var StatusLayer = cc.Layer.extend({
+    winsize : cc.winSize,
     ctor:function(){
         this._super();
         this.init();
     },
     init:function(){
         this._super();
-        var winsize = cc.winSize;
         var self = this;
         /**
         *先加载按钮plist文件
@@ -83,6 +83,11 @@ var StatusLayer = cc.Layer.extend({
             menuItem.y = 50;
             self.addChild(menuItem,10);
         });
+        /*
+        *升级界面创建与加载
+        *
+        */
+        this.createUpgradeMenu();
     },
     gemMenuItemCallback:function(){
         console.log('宝石')
@@ -97,6 +102,63 @@ var StatusLayer = cc.Layer.extend({
         console.log('任务')
     },
     upgradeMenuItemCallback:function(){
-        console.log('升级')
+        console.log('升级');
+
     },
+    createUpgradeMenu:function(){
+        var container = cc.Layer.create();
+        var scrollView = cc.ScrollView.create(cc.size(400,600));
+
+        // scrollView.setDirection(1);
+
+        scrollView.setTouchEnabled(true);
+
+        // scrollView.setBouncEnable(true);
+        
+        // scrollView.setSize(cc.size(winsize.width,200));
+        scrollView.x = this.winsize.width/2;
+        scrollView.y = this.winsize.height/2;
+        // scrollView.setContenSize(cc.size(winsize.width,6*200));
+
+        // scrollView.setAnchorPoint(cc.p(0.5,0.5));
+
+        // scrollView.setContentOffset(cc.p(0,200 - 6 * 200));
+         var boxW = 100, boxH = 100;
+        var box = new cc.LayerColor(cc.color(255, 255, 0, 255));
+        box.anchorX = 0;
+        box.anchorY = 0;
+        box.x = (this.winsize.width - boxW) / 2;
+        box.y = (this.winsize.height - boxH) / 2;
+        box.width = boxW;
+        box.height = boxH;
+
+        var markrside = 10.0;
+        var uL = new cc.LayerColor(cc.color(255, 0, 0, 255));
+        box.addChild(uL);
+        uL.width = markrside;
+        uL.height = markrside;
+        uL.x = 0;
+        uL.y = boxH - markrside;
+        uL.anchorX = 0;
+        uL.anchorY = 0;
+
+        var uR = new cc.LayerColor(cc.color(0, 0, 255, 255));
+        box.addChild(uR);
+        uR.width = markrside;
+        uR.height = markrside;
+        uR.x = boxW - markrside;
+        uR.y = boxH - markrside;
+        uR.anchorX = 0;
+        uR.anchorY = 0;
+
+
+        this.addChild(box);
+        // this.addChild(scrollView);
+       /* var pic1 = "res/image/pic/shop_item_bg_2.png"; 
+        var res = [pic1]
+        cc.loader.load(res, function(){
+            
+            container.addChild(cc.Sprite.create(pic1));
+        });*/
+    }
 });
